56dba945348d96aa90cd8ae10034bc3af4f9cb10
Philippe Massicotte <pmassicotte@hotmail.com>
2024-11-01 15:40:49 -0400

    fix(extract_nutrients.R): include additional column in selection to ensure complete data extraction

--- R/03_extract_nutrients.R
+++ R/03_extract_nutrients.R
@@ -39,7 +39,7 @@ batch_extract <- function(stations, downloaded_files) {
     ) |>
     select(-path) |>
     pivot_wider(names_from = data_type, values_from = raster_data) |>
-    select(1:4) |>
+    select(1:5) |>
     unnest(everything(), names_sep = "_")
 
   res
